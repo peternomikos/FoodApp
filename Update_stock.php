aa07abff695c55a0cca0a01872073147ffccef38
Pngiotis2018 <42089633+Pngiotis2018@users.noreply.github.com>
2018-08-17 19:14:08 +0300

    AAA

---
+++ Update_stock.php
@@ -0,0 +1,33 @@
+
+<?php
+ session_start();
+?>
+<html lang="gr">
+
+<head>
+  <title>Manager_View</title>
+</head>
+
+<?php
+
+$conn = mysqli_connect('localhost', 'root','','foodApp');
+$manager = $_SESSION['login_user'];
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+ if Cheesepie_Reserve='-',Simit_Reserve='-',Tost_Reserve='-',Cake_Reserve='-',Spinachpie_Reserve='-'{
+$sql = "UPDATE stock SET Cheesepie_Reserve='',Simit_Reserve='',Tost_Reserve='',Cake_Reserve='',Spinachpie_Reserve='' WHERE  Shop_Manager='$manager'";
+ }
+
+
+if (mysqli_query($con, $sql)) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
+
+
+
